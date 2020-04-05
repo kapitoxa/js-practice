f662eb4d37c8ebc915b455734e5ef53eb22f8ee7
Anton Borodaenko <k4p1toxa@gmail.com>
2020-04-05 11:20:55 +0300

    Решение задания 1

---
+++ React_01/script.js
@@ -0,0 +1,25 @@
+const employers = ['Alex', '', 'ludmila', 'Viktor', '', 'oleg', 'iNna', 'Ivan', 'Alex', 'Olga', ' Ann'];
+const employersNames = employers
+    .filter((item) => item.length > 0 && item.length != '')
+    .map((item) => item.toLowerCase().trim());
+
+const sponsors = {
+    cash: [40000, 5000, 30400, 12000],
+    eu: ['SRL', 'PLO', 'J&K'],
+    rus: ['RusAuto', 'SBO']
+};
+
+const {cash, eu, rus} = sponsors;
+
+const calcCash = (cash = 0) => cash.reduce((a, b) => a + b);
+const money = calcCash(cash);
+
+const makeBusiness = ({owner = 'Sam', director = 'Victor', cash, emp}) => {
+    const sumSponsors = [...eu, ...rus, 'unexpected sponsor'];
+    console.log(`We have a business. Owner: ${owner}, director: ${director}. Our budget: ${cash}. And our employers: ${emp}`);
+    console.log('And we have a sponsors: ');
+    console.log(`${sumSponsors}`);
+    console.log(`Note. Be careful with ${eu[0]}. It's a huge risk.`);
+};
+
+makeBusiness({cash: money, emp: employersNames});
