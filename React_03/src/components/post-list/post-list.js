4995966c264b834c03c696e2fa59fb2ee45e28d1
Anton Borodaenko <k4p1toxa@gmail.com>
2020-04-05 17:41:28 +0300

    Решение дополнительного задания

--- React_03/src/components/post-list/post-list.js
+++ React_03/src/components/post-list/post-list.js
@@ -5,14 +5,16 @@ import PostListItem from '../post-list-item';
 import './post-list.css';
 
 const PostList = ({posts}) => {
-    const elements = posts.map((item) => {
-        const {id, ...itemProps} = item;
-        return (
-            <li key={id} className="list-group-item">
-                <PostListItem {...itemProps} />
-            </li>
-        );
-    });
+    const elements = posts
+        .filter((item) => typeof item === 'object' && Object.keys(item).length > 0)
+        .map((item) => {
+            const {id, ...itemProps} = item;
+            return (
+                <li key={id} className="list-group-item">
+                    <PostListItem {...itemProps} />
+                </li>
+            );
+        });
 
     return (
         <ul className="app-list list-group">
