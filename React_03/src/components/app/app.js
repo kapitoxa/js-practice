887704c0adad67b3e6e59fcd7a9d036780e4b4d4
Anton Borodaenko <k4p1toxa@gmail.com>
2020-04-05 17:25:59 +0300

    Решение задания 3

---
+++ React_03/src/components/app/app.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import AppHeader from '../app-header';
+import SearchPanel from '../search-panel';
+import PostStatusFilter from '../post-status-filter';
+import PostList from '../post-list';
+import PostAddForm from '../post-add-form';
+
+import './app.css';
+
+const App = () => {
+    const data = [
+        {label: 'Going to learn React', important: true, id: 'qweqwe'},
+        {label: 'That is so good', important: false, id: 'bdfhbfgb'},
+        {label: 'I need break...', important: false, id: 'ksdfgj'}
+    ];
+
+    return (
+        <div className="app">
+            <AppHeader/>
+            <div className="search-panel d-flex">
+                <SearchPanel/>
+                <PostStatusFilter/>
+            </div>
+            <PostList posts={data}/>
+            <PostAddForm/>
+        </div>
+    );
+}
+
+export default App;
